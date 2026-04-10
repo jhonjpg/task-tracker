376f2cc016f047238f8aabd174078aa4233b082a
jhonjpg <jonathan050315jj@gmail.com>
2026-04-10 10:01:05 -0400

    Initial commit - Expo app created

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>estamos ready paara estae
+       yes uyes!</Text>
+      <Text style={styles.subtitle}>Empieza a programar tu App de Tareas aquí.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5', // Un gris muy claro de fondo
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
-});
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+  },
+}); 
